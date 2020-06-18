267d7c25b11fd368aaf4546f26b59255f2f09b1a
Raul <raulcc50@gmail.com>
2020-06-18 16:15:24 -0300

    feat: Funcionalidade de atualização da cotação de ações

--- backend/grails-app/services/backend/CompanyService.groovy
+++ backend/grails-app/services/backend/CompanyService.groovy
@@ -1,34 +1,53 @@
 package backend
 
-import javax.transaction.Transactional
+import grails.gorm.transactions.Transactional
 import java.time.LocalDateTime
 
 @Transactional
 class CompanyService {
 
+    static scope = "session"
+    Double initialPrice = 1.00d
+
+    def count(){
+        Company.count()
+    }
+
     def get(id){
-        Company.get(id)
+        def company = Company.get(id)
+        return company
     }
 
     def list(){
-        Company.list()
+        def read = Company.list()
+        return read
+    }
+
+    def update(){
+        def listCompanies = list()
+        for (int i = 0; i < listCompanies.size(); i++) {
+            listCompanies[i].addToStocks(new Stock(price: this.initialPrice, datePrice: LocalDateTime.now()))
+            listCompanies[i].save()
+        }
+        this.initialPrice += 0.02
+        if (this.initialPrice.equals(2.00)){
+            this.initialPrice = 1.00d
+        }
+        println("Updating Stock Quotes")
     }
 
     def save(Company company){
-        Stock initialQuota = new Stock(price: 1.12d,datePrice: LocalDateTime.now())
+        Double price = 1.00
         company = new Company(
                 name: company.name,
                 segment: company.segment
         )
-        for (int i; i<3; i++){
-            Double newPrice = initialQuota.getPrice() + 0.10d
-            company.addToStocks(new Stock(price: newPrice,datePrice: LocalDateTime.now()))
-            initialQuota.setPrice(newPrice)
-        }
+        company.addToStocks(new Stock(price: price + 0.10d, datePrice: LocalDateTime.now()))
         company.save()
     }
 
     def delete(id){
         Company.get(id).delete()
     }
+
 }
