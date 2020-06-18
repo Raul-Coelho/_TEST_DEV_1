a50b17db8b91a2bb569259c7df0f95fd25cf3823
Raul <raulcc50@gmail.com>
2020-06-18 16:15:57 -0300

    refact: criando e verificando horario comercial

--- backend/grails-app/init/backend/BootStrap.groovy
+++ backend/grails-app/init/backend/BootStrap.groovy
@@ -1,22 +1,20 @@
 package backend
 
 import enumeration.Segments
-
-import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
 
 class BootStrap {
 
 //    Check Trade Time
-    LocalTime startTradeTime = LocalTime.of(15,25,00)
-    LocalTime endTradeTime = LocalTime.of(15,26,00)
+    LocalTime startTradeTime = LocalTime.of(10,00,00)
+    LocalTime endTradeTime = LocalTime.of(18,0,00)
 
-    long tradeTime = ChronoUnit.MINUTES.between(startTradeTime,endTradeTime)
+    long tradeTime = ChronoUnit.SECONDS.between(startTradeTime,endTradeTime)
 
-    Stock initialQuota = new Stock(price: 1.12d,datePrice: LocalDateTime.now())
-
-    CompanyController companyController
     CompanyService service = new CompanyService()
 
     def init = { servletContext ->
@@ -36,7 +34,22 @@ class BootStrap {
                 segment: Segments.OIL,
         ))
 
+        ScheduledFuture<?> f = Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+            @Override
+            void run() {
+                if (startTradeTime != endTradeTime){
+                    service.update()
+                    println(LocalTime.now())
+                    setStartTradeTime(startTradeTime.plusHours(1))
+                    println(startTradeTime)
+                }else{
+                    println("FORA DO HORARIO COMERCIAL")
+                }
+            }
+        }, 2, 60000, TimeUnit.MILLISECONDS)
+        Thread.sleep(10000)
     }
+
     def destroy = {
     }
 }
